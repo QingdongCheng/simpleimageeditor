ab80900dbaab67b06175517b2a2fb5a61a1c61ea
cnit355project <34171535+cnit355project@users.noreply.github.com>
2017-12-04 12:48:02 -0500

    Added brightness, contrast and saturation adjustments

--- app/src/main/java/edu/purdue/qingdong/simpleimageeditor/EditActivity.java
+++ app/src/main/java/edu/purdue/qingdong/simpleimageeditor/EditActivity.java
@@ -1,5 +1,6 @@
 package edu.purdue.qingdong.simpleimageeditor;
 
+import android.app.Dialog;
 import android.content.ActivityNotFoundException;
 import android.content.ContentResolver;
 import android.content.DialogInterface;
@@ -22,11 +23,14 @@ import android.os.Bundle;
 import android.text.InputType;
 import android.util.DisplayMetrics;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.SeekBar;
 import android.widget.Toast;
 
 import java.io.ByteArrayOutputStream;
@@ -58,14 +62,15 @@ public class EditActivity extends AppCompatActivity {
     ImageView editImageView;
     String imagePath;
     String grayPhotoPath;
+    String brightnessPhotoPath;
+    String contrastPhotoPath;
+    String saturationPhotoPath;
     private static final int REQUEST_CROP = 1;
 
     private CallbackManager callbackManager;
     private ShareDialog shareDialog;
     private Bitmap bitmap;
     private Bitmap bitmapAddText;
-
-
 
     private DataBaseHandler db;
 
@@ -75,7 +80,6 @@ public class EditActivity extends AppCompatActivity {
         setContentView(R.layout.activity_edit);
 
         db = new DataBaseHandler(this);
-
 
         editImageView = (ImageView) findViewById(R.id.editImageView);
         imagePath = getIntent().getStringExtra("imagePath");
@@ -95,6 +99,351 @@ public class EditActivity extends AppCompatActivity {
         initFacebook();
     }
 
+    float brightness;
+    float contrast;
+    float saturation;
+
+    public void onClickBrightness(View view)
+    {
+        dialogBrightness();
+    }
+
+    public void onClickContrast(View view)
+    {
+        dialogContrast();
+    }
+
+    public void onClickSaturation(View view)
+    {
+        dialogSaturation();
+    }
+
+    public void dialogBrightness()
+    {
+        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        final SeekBar adjustment = new SeekBar(this);
+        adjustment.setMax(255);
+        adjustment.setKeyProgressIncrement(1);
+
+        builder.setTitle("Brightness");
+        builder.setMessage("Drag around to adjust brightness.");
+        builder.setView(adjustment);
+        builder.setPositiveButton("Confirm", new DialogInterface.OnClickListener()
+        {
+            public void onClick(DialogInterface dialog, int which)
+            {
+                Bitmap bitmap;
+                if (imagePath != null) {
+                    bitmap = BitmapFactory.decodeFile(imagePath);
+                } else {
+                    bitmap= BitmapFactory.decodeResource(getResources(), R.drawable.android);
+                }
+
+                Bitmap brightnessBmp = adjustBrightness(bitmap, brightness);
+                File brightnessPhotoFile = null;
+                try {
+                    brightnessPhotoFile = createSaturationImageFile();
+                } catch (IOException e) {
+
+                }
+
+                if (brightnessPhotoFile != null) {
+                    try {
+                        FileOutputStream out = new FileOutputStream(brightnessPhotoFile);
+                        brightnessBmp.compress(Bitmap.CompressFormat.JPEG, 90, out);
+                        out.flush();
+                        out.close();
+
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+
+                }
+
+                BitmapFactory.Options options = new BitmapFactory.Options();
+                options.inSampleSize = 2;      // 1/4 of original image
+                Bitmap b = BitmapFactory.decodeFile(brightnessPhotoPath, options);
+                editImageView.setImageBitmap(b);
+                imagePath = brightnessPhotoPath;
+            }
+        }).setNegativeButton("Cancel", new DialogInterface.OnClickListener()
+        {
+            public void onClick(DialogInterface dialog, int which)
+            {
+                dialog.cancel();
+            }
+        }
+        );
+        builder.show();
+
+        adjustment.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser)
+            {
+                if (fromUser)
+                {
+                    brightness = (float) progress;
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
+
+    public void dialogContrast()
+    {
+        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        final SeekBar adjustment = new SeekBar(this);
+        adjustment.setMax(255);
+        adjustment.setKeyProgressIncrement(1);
+
+        builder.setTitle("Contrast");
+        builder.setMessage("Drag around to adjust contrast.");
+        builder.setView(adjustment);
+        builder.setPositiveButton("Confirm", new DialogInterface.OnClickListener()
+        {
+            public void onClick(DialogInterface dialog, int which)
+            {
+                Bitmap bitmap;
+                if (imagePath != null) {
+                    bitmap = BitmapFactory.decodeFile(imagePath);
+                } else {
+                    bitmap= BitmapFactory.decodeResource(getResources(), R.drawable.android);
+                }
+
+                Bitmap contrastBmp = adjustContrast(bitmap, contrast);
+                File contrastPhotoFile = null;
+                try {
+                    contrastPhotoFile = createSaturationImageFile();
+                } catch (IOException e) {
+
+                }
+
+                if (contrastPhotoFile != null) {
+                    try {
+                        FileOutputStream out = new FileOutputStream(contrastPhotoFile);
+                        contrastBmp.compress(Bitmap.CompressFormat.JPEG, 90, out);
+                        out.flush();
+                        out.close();
+
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+
+                }
+
+                BitmapFactory.Options options = new BitmapFactory.Options();
+                options.inSampleSize = 2;      // 1/4 of original image
+                Bitmap b = BitmapFactory.decodeFile(contrastPhotoPath, options);
+                editImageView.setImageBitmap(b);
+                imagePath = contrastPhotoPath;
+            }
+        }).setNegativeButton("Cancel", new DialogInterface.OnClickListener()
+        {
+             public void onClick(DialogInterface dialog, int which)
+             {
+                 dialog.cancel();
+             }
+        }
+        );
+        builder.show();
+
+        adjustment.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser)
+            {
+                if (fromUser)
+                {
+                    contrast = (float) progress;
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
+
+    public void dialogSaturation()
+    {
+        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        final SeekBar adjustment = new SeekBar(this);
+        adjustment.setMax(255);
+        adjustment.setKeyProgressIncrement(1);
+
+        builder.setTitle("Saturation");
+        builder.setMessage("Drag around to adjust saturation.");
+        builder.setView(adjustment);
+        builder.setPositiveButton("Confirm", new DialogInterface.OnClickListener()
+        {
+            public void onClick(DialogInterface dialog, int which)
+            {
+                Bitmap bitmap;
+                if (imagePath != null) {
+                    bitmap = BitmapFactory.decodeFile(imagePath);
+                } else {
+                    bitmap= BitmapFactory.decodeResource(getResources(), R.drawable.android);
+                }
+
+                Bitmap saturationBmp = adjustSaturation(bitmap, saturation);
+                File saturationPhotoFile = null;
+                try {
+                    saturationPhotoFile = createSaturationImageFile();
+                } catch (IOException e) {
+
+                }
+
+                if (saturationPhotoFile != null) {
+                    try {
+                        FileOutputStream out = new FileOutputStream(saturationPhotoFile);
+                        saturationBmp.compress(Bitmap.CompressFormat.JPEG, 90, out);
+                        out.flush();
+                        out.close();
+
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+
+                }
+
+                BitmapFactory.Options options = new BitmapFactory.Options();
+                options.inSampleSize = 2;      // 1/4 of original image
+                Bitmap b = BitmapFactory.decodeFile(saturationPhotoPath, options);
+                editImageView.setImageBitmap(b);
+                imagePath = saturationPhotoPath;
+            }
+        }).setNegativeButton("Cancel", new DialogInterface.OnClickListener()
+        {
+            public void onClick(DialogInterface dialog, int which)
+            {
+                dialog.cancel();
+            }
+        });
+        builder.show();
+
+        adjustment.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser)
+            {
+                if (fromUser)
+                {
+                    saturation = (float) progress;
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
+
+    public Bitmap adjustSaturation(Bitmap bmpOriginal, float saturation)
+    {
+        int width, height;
+        height = bmpOriginal.getHeight();
+        width = bmpOriginal.getWidth();
+        Bitmap bmpSaturation = Bitmap.createBitmap(width, height, bmpOriginal.getConfig());
+        Canvas c = new Canvas(bmpSaturation);
+        Paint paint = new Paint();
+        ColorMatrix cm = new ColorMatrix();
+        cm.setSaturation(saturation);
+        ColorMatrixColorFilter f = new ColorMatrixColorFilter(cm);
+        paint.setColorFilter(f);
+        c.drawBitmap(bmpOriginal, 0, 0, paint);
+        return bmpSaturation;
+    }
+
+    public Bitmap adjustBrightness(Bitmap bmpOriginal, float brightness)
+    {
+        ColorMatrix cm = new ColorMatrix(new float[]
+                {
+                        0, 0, 0, 0, brightness,
+                        0, 0, 0, 0, brightness,
+                        0, 0, 0, 0, brightness,
+                        0, 0, 0, 1, 0
+                });
+
+        Bitmap bmpBrightness = Bitmap.createBitmap(bmpOriginal.getWidth(), bmpOriginal.getHeight(), bmpOriginal.getConfig());
+        Canvas canvas = new Canvas(bmpBrightness);
+        Paint paint = new Paint();
+        paint.setColorFilter(new ColorMatrixColorFilter(cm));
+        canvas.drawBitmap(bmpOriginal, 0, 0, paint);
+        return bmpBrightness;
+    }
+
+    public static Bitmap adjustContrast(Bitmap bmpOriginal, float contrast)
+    {
+        ColorMatrix cm = new ColorMatrix(new float[]
+                {
+                        contrast, 0, 0, 0, 0,
+                        0, contrast, 0, 0, 0,
+                        0, 0, contrast, 0, 0,
+                        0, 0, 0, 1, 0
+                });
+
+        Bitmap bmpContrast = Bitmap.createBitmap(bmpOriginal.getWidth(), bmpOriginal.getHeight(), bmpOriginal.getConfig());
+        Canvas canvas = new Canvas(bmpContrast);
+        Paint paint = new Paint();
+        paint.setColorFilter(new ColorMatrixColorFilter(cm));
+        canvas.drawBitmap(bmpOriginal, 0, 0, paint);
+        return bmpContrast;
+    }
+
+    private File createSaturationImageFile() throws IOException
+    {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "Saturation_" + timeStamp + "_.jpg";
+        File image = new File(Environment.getExternalStorageDirectory(),
+                imageFileName);
+        // Save a file: path for use with ACTION_VIEW intents
+        saturationPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+    private File createBrightnessImageFile() throws IOException
+    {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "Brightness_" + timeStamp + "_.jpg";
+        File image = new File(Environment.getExternalStorageDirectory(),
+                imageFileName);
+        // Save a file: path for use with ACTION_VIEW intents
+        brightnessPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+    private File createContrastImageFile() throws IOException
+    {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "Contrast_" + timeStamp + "_.jpg";
+        File image = new File(Environment.getExternalStorageDirectory(),
+                imageFileName);
+        // Save a file: path for use with ACTION_VIEW intents
+        contrastPhotoPath = image.getAbsolutePath();
+        return image;
+    }
 
     public void onCrop(View v) {
 
